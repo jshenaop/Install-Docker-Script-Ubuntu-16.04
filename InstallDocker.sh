5b35c6a2f914bd67ad6be0fa0cdf06098a1734aa
jshenaop <jshenaop@hotmail.com>
2019-05-09 16:22:06 -0500

    first commit

---
+++ InstallDocker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Install Docker CE to Ubuntu-16.04
+
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
+
+
+# Install Docker-Compose to Ubuntu-16.04
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
